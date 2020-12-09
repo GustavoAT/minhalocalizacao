ac3d410052f44a85e286cd5ff62d87dbfae10865
Gustavo <gustavoalmeidateixeira@gmail.com>
2020-12-08 22:32:28 -0300

    função de localização atual

--- app/src/main/java/com/gustavoat/minhalocalizacao/MapsActivity.java
+++ app/src/main/java/com/gustavoat/minhalocalizacao/MapsActivity.java
@@ -1,28 +1,47 @@
 package com.gustavoat.minhalocalizacao;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker myPosition;
+    private static final int ACCESS_FINE_LOCATION_REQUEST = 1;
+    TextView txtLatLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        txtLatLon = findViewById(R.id.textViewLatitude);
+        txtLatLon.setText(R.string.latitude_label);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     /**
@@ -37,14 +56,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        requestPermissions();
 
-        // Add a marker in user's position and move the camera
-        LatLng here = new LatLng(-12.7014, -38.3214);
-        mMap.addMarker(new MarkerOptions().position(here).title("Marker in here"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(here, 16));
+        myPosition = null;
     }
 
     public void requestPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATION_REQUEST);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == ACCESS_FINE_LOCATION_REQUEST) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startPositionService();
+            } else {
+                Toast.makeText(this, getText(R.string.error_permission),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void startPositionService(){
+        try {
+            LocationManager locationmanager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            LocationListener locationlistener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                     refreshPosition(location);
+                }
+            };
+            locationmanager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    0, 0, locationlistener);
+        } catch (SecurityException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void refreshPosition(Location location){
+        LatLng newPosition = new LatLng(location.getLatitude(), location.getLongitude());
+        if(myPosition == null){
+            myPosition = mMap.addMarker(new MarkerOptions()
+                    .position(newPosition)
+                    .title(getString(R.string.you_are_here)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition, 16));
+        }else {
+            myPosition.setPosition(newPosition);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(newPosition));
+        }
+        String latitude = String.format("%1$s %2$.4f\n%3$s %4$.4f",
+                getText(R.string.latitude_label),
+                newPosition.latitude,
+                getText(R.string.longitude_label),
+                newPosition.longitude);
+        txtLatLon.setText(latitude);
     }
 }
